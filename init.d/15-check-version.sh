4d866f6f473a95d2f86e433da568cf1beaff3836
shado <shadowsvoices@gmail.com>
2017-06-30 23:15:48 +0200

    fixed version check

--- init.d/15-check-version.sh
+++ init.d/15-check-version.sh
@@ -4,7 +4,7 @@ set -e
 echo "INSTALLED_VERSION=${INSTALLED_VERSION}"
 echo "SEAFILE_VERSION=${SEAFILE_VERSION}"
 
-if [ $INSTALLED_VERSION != $SEAFILE_VERSION ]; then
+if [ $INSTALLED_VERSION != "NONE" && $INSTALLED_VERSION != $SEAFILE_VERSION ]; then
 	echo "ERROR: INSTALLED_VERSION=${INSTALLED_VERSION} does not match SEAFILE_VERSION=${SEAFILE_VERSION} installed in container"
 	echo "Manual intervention for upgrade is required."
 	exit 31
